af16f9109e1aeb9875fe863b8aeaf962a0508f45
MikhailEA <michev.work@gmail.com>
2021-08-30 16:07:37 +0300

    Добавление фрагмента исправление

--- app/src/main/java/com/android/fragmentkotlin/FirstFragment.kt
+++ app/src/main/java/com/android/fragmentkotlin/FirstFragment.kt
@@ -34,7 +34,7 @@ class FirsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_firs, container, false)
+        return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
     companion object {
