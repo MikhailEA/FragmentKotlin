7f8bec236d79a1193cb19d004f599ed48d17f521
MikhailEA <michev.work@gmail.com>
2021-08-30 18:36:54 +0300

    Редактирование фрагмента и добавление второго

--- app/src/main/java/com/android/fragmentkotlin/SecondFragment.kt
+++ app/src/main/java/com/android/fragmentkotlin/SecondFragment.kt
@@ -6,6 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class FirsFragment : Fragment(R.layout.fragment_first) {
+class SecondFragment : Fragment(R.layout.fragment_second) {
 
 }
