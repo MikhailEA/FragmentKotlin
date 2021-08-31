6f7ae24c88529a3aab21f32b780435b36cba88fb
MikhailEA <michev.work@gmail.com>
2021-08-31 18:46:59 +0300

    Реализация

--- app/src/main/java/com/android/fragmentkotlin/MainActivity.kt
+++ app/src/main/java/com/android/fragmentkotlin/MainActivity.kt
@@ -7,5 +7,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firsFragment = FirsFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firsFragment)
+            commit()
+        }
+
+        btnFragment1.setOnclickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firsFragment)
+                commit()
+            }
+        }
+
     }
 }
