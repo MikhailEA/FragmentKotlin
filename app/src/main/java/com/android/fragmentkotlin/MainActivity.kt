bf2abfdb4859fe5072c71576537503b7a4efc70b
MikhailEA <michev.work@gmail.com>
2021-09-01 17:59:09 +0300

    Переключение кнопок

--- app/src/main/java/com/android/fragmentkotlin/MainActivity.kt
+++ app/src/main/java/com/android/fragmentkotlin/MainActivity.kt
@@ -1,27 +1,47 @@
 package com.android.fragmentkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnFragment1 = findViewById<Button>(R.id.btnFragment1)
+        var btnFragment2 = findViewById<Button>(R.id.btnFragment2)
 
         val firsFragment = FirsFragment()
         val secondFragment = SecondFragment()
 
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.flFragment, firsFragment)
+            addToBackStack(null)
             commit()
         }
 
-        btnFragment1.setOnclickListener {
+
+        btnFragment1.setOnClickListener{
             supportFragmentManager.beginTransaction().apply {
                 replace(R.id.flFragment, firsFragment)
                 commit()
             }
         }
+
+        btnFragment2.setOnClickListener{
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                commit()
+            }
+        }
+
+
+
+
+
 
     }
 }
